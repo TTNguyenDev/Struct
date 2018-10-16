67831cd2649f273222448812f322ed1950b09730
TTNguyen <nttriet9a4@gmail.com>
2018-10-16 10:56:40 +0700

    add Search Functionality

--- Struct_CTDL/main.cpp
+++ Struct_CTDL/main.cpp
@@ -1,30 +1,63 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 struct student {
     string fullName;
     string ID;
-    int credit;
 };
 
-void inputStudent(student &A) {
-    cout << "Enter your name: ";
-    getline(cin, A.fullName);
-    cout << "Enter your ID: ";
-    getline(cin, A.ID);
-    cout << "Enter your credit that you studied: ";
-    cin >> A.credit;
+void inputStudent(vector<student> &stu, int n) {
+    for (int i = 0; i < n; i++) {
+        student temp;
+        cout << "Enter your name: ";
+        getline(cin, temp.fullName);
+        cout << "Enter your ID: ";
+        getline(cin, temp.ID);
+        
+        stu.push_back(temp);
+    }
 }
 
-void printStudent(student A) {
-    cout << "Name  : " << A.fullName << endl;
-    cout << "ID    : " << A.ID << endl;
-    cout << "Credit: " << A.credit << endl;
+string sequentialSearchStudent(vector<student> &stu, string x) {
+    for (int i = 0; i < stu.size(); i++)
+        if (stu[i].ID == x)
+            return stu[i].fullName;
+    return "-1";
+}
+
+string binarySearchStudent(vector<student> &stu, string x) {
+    int l = 0;
+    int r = stu.size() - 1;
+    while (l <= r) {
+        int m = (l+r)/2;
+        
+        if (stu[m].ID == x)
+            return stu[m].fullName;
+        if (stu[m].ID< x)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return "-1";
+}
+
+void printStudent(vector<student> &stu) {
+    for (int i = 0; i < stu.size(); i++) {
+        cout << "Name  : " << stu[i].fullName << endl;
+        cout << "ID    : " << stu[i].ID << endl;
+    }
 }
 
 int main() {
-    student a;
-    inputStudent(a);
-    printStudent(a);
+    int n;
+    cout << "Enter number of student: ";
+    cin >> n;
+    cin.ignore();
+    vector<student> stu;
+    inputStudent(stu, n);
+    printStudent(stu);
+    cout << sequentialSearchStudent(stu, "1753113");
+    cout << binarySearchStudent(stu, "1753113");
+    
 }
